970daca771aee695c961e3ada2d9b6fd3f9d18fe
Shivanirathor <shivanirathor034@gmail.com>
2023-06-02 22:26:22 +0530

    pic

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,46 @@
-import React from 'react'
-import '../styles/App.css';
-import { Loader } from './Loader';
-import { PhotoFrame } from './PhotoFrame';
-const App = () => {
-  
-}
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Loader from './Loader';
+import PhotoFrame from './PhotoFrame';
 
+const App = () => {
+  const [number, setNumber] = useState('');
+  const [photoData, setPhotoData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleInputChange = (e) => {
+    setNumber(e.target.value);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (number !== '') {
+        setIsLoading(true);
+
+        try {
+          const response = await fetch(`https://jsonplaceholder.typicode.com/photos/${number}`);
+          const data = await response.json();
+          setPhotoData(data);
+        } catch (error) {
+          console.log('Error:', error);
+        }
+
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [number]);
+
+  return (
+    <div className="App">
+      <input type="number" value={number} onChange={handleInputChange} placeholder="Enter a number" />
+
+      {isLoading && <Loader />}
+
+      {photoData && <PhotoFrame url={photoData.url} title={photoData.title} />}
+    </div>
+  );
+};
 
 export default App;
--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export const PhotoFrame = () => {
-   
-}
+const PhotoFrame = ({ url, title }) => {
+  return (
+    <div className="photoframe">
+      <img src={url} alt={title} />
+      <div className="caption">{title}</div>
+    </div>
+  );
+};
+
+export default PhotoFrame;
